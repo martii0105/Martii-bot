663268ff930f01ffebb2686ebcb4d048cc162564
martii0105 <128637734+martii0105@users.noreply.github.com>
2023-03-22 21:39:26 -0300

    Update index-inicio.js

--- index-inicio.js
+++ index-inicio.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('THE DORRAT-BOT-MD', {
+say('Martii-bot', {
 font: 'tiny',
 align: 'center',
 colors: ['#ff8000']})
-say(`CREADO POR DIEGO-OFC`, {
+say(`CREADO POR Marti`, {
 font: 'console',
 align: 'center',
 colors: ['red']})
